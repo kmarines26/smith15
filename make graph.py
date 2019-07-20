ece8ab3d3b783897b8b10efe93a77640527249f9
kmarines26 <52984930+kmarines26@users.noreply.github.com>
2019-07-20 13:39:13 +0900

    Create make graph.py

---
+++ make graph.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+import cv2
+
+import os
+
+
+#screen_capsというフォルダが作成され, 動画の長さに応じて60fpsのレートでpngファイルが保存される
+
+#0.pngから順に99.pngまでimreadし, 1次元配列yにそれぞれの画像の輝度値の平均値を代入する.
+
+im0 = imread('screen_caps/0.png')
+
+im1 = imread('screen_caps/1.png')
+
+im2 = imread('screen_caps/2.png')
+
+im3 = imread('screen_caps/3.png')
+
+im4 = imread('screen_caps/4.png')
+
+im5 = imread('screen_caps/5.png')
+
+im6 = imread('screen_caps/6.png')
+
+im7 = imread('screen_caps/7.png')
+
+im8 = imread('screen_caps/8.png')
+
+im9 = imread('screen_caps/9.png')
+
+im10 = imread('screen_caps/10.png')
+
+im11 = imread('screen_caps/11.png')
+
+im12 = imread('screen_caps/12.png')
+
+im13 = imread('screen_caps/13.png')
+
+im14 = imread('screen_caps/14.png')
+
+im15 = imread('screen_caps/15.png')
+
+im16 = imread('screen_caps/16.png')
+
+im17 = imread('screen_caps/17.png')
+
+im18 = imread('screen_caps/18.png')
+
+im19 = imread('screen_caps/19.png')
+
+im20 = imread('screen_caps/20.png')
+
+im21 = imread('screen_caps/21.png')
+
+im22 = imread('screen_caps/22.png')
+
+im23...
